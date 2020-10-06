8c09ba54449a4ea325039ab2088f8f6b20c9b8d1
SpideyFanDan <dsmith.teach@gmail.com>
2020-10-05 21:16:55 -0600

    wrote about page text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,41 @@ import React, { Component } from 'react';
 export default class About extends Component {
 	render() {
 		return (
+			<div>
 				<h1>ABOUT PAGE...coming soon</h1>
+				<p>
+					My name is Dan Smith, and I've always felt that where my name lacks in
+					excitement, I have to make up for it with personality.{' '}
+				</p>
+				<p>
+					I am an adventurous soul with a child-like nature. I love comic books
+					and particularly Spider-Man comics that I have read since I was a
+					child. My first software development app that I ever created using
+					HTML, CSS, and JavaScript was a trivia app about the legend himself.
+				</p>
+				<p>
+					I also love to travel and speak several languages such as Italian,
+					French, and Spanish. I have lived in countries where those languages
+					are spoken and travelled to many more. While in Graduate School to
+					become a teacher I won the original iPhone from a travel blog I wrote
+					back in the summer of 2007 and continued to write more to that blog
+					because I find it fun and therapeutic.
+				</p>
+				<p>
+					I also ride my bike as a predominant means of transportation, partly
+					to save money on other means of travel, but mostly to reduce my carbon
+					footprint. I recently moved to Denver, Colorado in order to enjoy a
+					new adventure in life and I can't wait to get a mountain bike so I can
+					add new blog posts to my travel experiences as I find new trails to
+					ride on in the outdoorsy city and state.
+				</p>
+				<p>
+					Take a look at my website. I've posted my portfolio of web
+					applications I've created, my resume, and my travel blog which also
+					includes my award-winning post about my study abroad in Brazil. If you
+					are looking to connect, please
+				</p>
+			</div>
 		);
 	}
 }
