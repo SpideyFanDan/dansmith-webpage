e9eb580ad24d56f47fe8e92501a769761c60ea23
SpideyFanDan <dsmith.teach@gmail.com>
2020-10-14 14:42:56 -0600

    ready to redeploy with Resume page updated and viewable

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,39 +1,35 @@
 import React, { Component } from 'react';
-import { MDBBtn } from 'mdbreact';
-import { Document, Page } from 'react-pdf';
-import resumePDF from './dan-smith-resume-2020.pdf';
-// import 'react-pdf/dist/Page/AnnotationLayer.css';
+import { Button, Image } from 'react-bootstrap';
 class Resume extends Component {
-	state = { numPages: null, pageNumber: 1 };
+	// state = { numPages: null, pageNumber: 1 };
 
-	onDocumentLoadSuccess = ({ numPages }) => {
-		this.setState({ numPages });
-	};
+	// onDocumentLoadSuccess = ({ numPages }) => {
+	// 	this.setState({ numPages });
+	// };
 
-	goToPrevPage = () =>
-		this.setState((state) => ({ pageNumber: state.pageNumber - 1 }));
-	goToNextPage = () =>
-		this.setState((state) => ({ pageNumber: state.pageNumber + 1 }));
+	// goToPrevPage = () =>
+	// 	this.setState((state) => ({ pageNumber: state.pageNumber - 1 }));
+	// goToNextPage = () =>
+	// 	this.setState((state) => ({ pageNumber: state.pageNumber + 1 }));
 	render() {
-		const { pageNumber, numPages } = this.state;
+		// const { pageNumber, numPages } = this.state;
 		return (
 			<div>
 				<a
 					target='new'
 					href='https://drive.google.com/file/d/1ZoYpvnoSyXHyXHGc0F2ALXjO43VyrN_l/view?usp=sharing'
-					className='email-me'>
-					<MDBBtn
-						className='white-text'
-						rounded
-						size='lg'
-						color='mdb-color darken-1'>
+					className='view-button'>
+					<Button className='view-button' rounded variant='gray'>
 						Click here to download or view below
-					</MDBBtn>
+					</Button>
 				</a>
 				<hr />
 				<h2>Resume</h2>
-				<div style={{ width: 600 }}>
-					<img src='../images/dansmith-resume.jpg' alt='Dan Smith resume'></img>
+				<div>
+					<img
+						className='resume-img'
+						src={require('../images/dansmith-resume.jpg')}
+						alt='Dan Smith resume'></img>
 				</div>
 			</div>
 		);
